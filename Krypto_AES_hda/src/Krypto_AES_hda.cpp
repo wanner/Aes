e95d388fcc8660ad2969d15058cacbbd8a5a3ee8
JPfndrt <jp>
2014-05-17 15:07:39 +0200

    first initial test

---
+++ Krypto_AES_hda/src/Krypto_AES_hda.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Krypto_AES_hda.cpp
+// Author      : JPfndrt
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
